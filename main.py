816f832318eda0236980b30701ba11b9920cea4b
Pom <95515205+ppompompurin@users.noreply.github.com>
2023-04-09 22:53:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+# DO NOT MODIFY ANYTHING BELOW IF YOU DO NOT KNOW WHAT YOU ARE DOING!!!
+
+
+
+
+
+# MADE WITH LOVE BY NOAHREPUBLIC, J3LDO, SLOURN <3, POMPOMPURIN, AND DUCKYY
 import requests as r
 from threading import Thread
 import os
 import uuid
 import time
 import datetime
+import json
+import colorama
 
+os.system("color 02")
+os.system("title made with love by noahrepublic, J3LDO, slourn, and pompompurin")
 
-print("Checking for updates...")
+with open("limiteds.txt", "r") as f:
+    limiteds = f.read().replace(" ", "").split(",")
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+    discordWebhook = config['discordWebhook']
+
+print(f">> [LOGS] CHECKING FOR UPDATES")
 script = r.get("https://raw.githubusercontent.com/ppompompurin/roblox-free-ugc-sniper/main/main.py").text
 with open("main.py", "r") as f:
     if f.read() != script:
@@ -16,14 +34,37 @@ with open("main.py", "r") as f:
             input("Updated please reopen the script")
             exit(0)
 
-            
+
+
+try:
+    import winreg
+
+    path = winreg.HKEY_CURRENT_USER
+    robloxcom = winreg.OpenKeyEx(path, r"SOFTWARE\\Roblox\\RobloxStudioBrowser\\roblox.com")
+
+
+
+    cookie = str(winreg.QueryValueEx(robloxcom, ".ROBLOSECURITY")[0]) 
+except:
+    print(">> [LOGS] Failed to autodetect a cookie.")
+    cookie = None
+
+
+if cookie:
+    cookie = cookie.split("<")[3]
+    cookie = cookie.split(">")[0]
+    cookie = cookie.strip()
+
+    try:
+        user_id = r.get("https://users.roblox.com/v1/users/authenticated", cookies={".ROBLOSECURITY": cookie}).json()["id"]
+        print(">> [LOGS] SUCCESSFULLY AUTOMATICALLY FOUND COOKIE")
+    except:
+        cookie = config['cookie']
+else:
+    cookie = config['cookie']
 
 with open("limiteds.txt", "r") as f:
     limiteds = f.read().replace(" ", "").split(",")
-
-with open("cookie.txt", "r") as f:
-    cookie = f.read()
-
 
 user_id = r.get("https://users.roblox.com/v1/users/authenticated", cookies={".ROBLOSECURITY": cookie}).json()["id"]
 x_token = ""
@@ -32,7 +73,7 @@ def get_x_token():
 
     x_token = r.post("https://auth.roblox.com/v2/logout",
                      cookies={".ROBLOSECURITY": cookie}).headers["x-csrf-token"]
-    print("Logged in.")
+    print(">> [LOGS] LOGGED IN SUCCESSFULLY")
 
     while 1:
         # Gets the x_token every 4 minutes.
@@ -42,7 +83,7 @@ def get_x_token():
 
 
 def buy(json, itemid, productid):
-    print("Spam buying limited...")
+    print(">> [LOGS] SPAM BUYING LIMITED")
 
     data = {
         "collectibleItemId": itemid,
@@ -62,7 +103,7 @@ def buy(json, itemid, productid):
             headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie})
 
         if bought.reason == "Too Many Requests":
-            print("Ran into a ratelimit resuming trying again shortly...")
+            print(">> [LOGS] RATELIMITED TOO MANY REQUESTS, TRYING AGAIN")
             time.sleep(0.5)
             continue
 
@@ -70,13 +111,15 @@ def buy(json, itemid, productid):
             bought = bought.json()
         except:
             print(bought.reason)
-            print("Json decoder error whilst trying to buy item.")
+            print(">> [LOGS] JSON DECODING ERROR WHILE TRYING TO BUY")
             continue
 
         if not bought["purchased"]:
-            print(f"Failed buying the limited, trying again.. Info: {bought} - {data}")
+            print(f">> [LOGS] FAILED BUYING LIMITED, TRYING AGAIN - {bought} - {data}")
         else:
-            print(f"Successfully bought the limited! Info: {bought} - {data}")
+            print(f">> [LOGS] SUCCESSFULLY PURCHASED LIMITED - {bought} - {data}")
+            r.post(discordWebhook, data={"content": f">> [LOGS] PURCHASED FREE LIMITED - {bought} - {data}"})
+            
 
         try:
             info = r.post("https://catalog.roblox.com/v1/catalog/items/details",
@@ -86,21 +129,21 @@ def buy(json, itemid, productid):
             info = {"unitsAvailableForConsumption": 1}
 
         if info["unitsAvailableForConsumption"] == 0:
-            print("Couldn't buy the limited in time. Better luck next time.")
+            print(">> [LOGS] COULD NOT BUY IN TIME SADLY")
             return
 
 
 # Get collectible and product id for all the limiteds.
 Thread(target=get_x_token).start()
 
-print("UGC Sniper made by Jeldo#9587 and modifications done by pompompurin#9999 \nDiscord server: https://discord.gg/XX6qtbvkHG")
+
 while x_token == "":
     time.sleep(0.01)
 
 # https://apis.roblox.com/marketplace-items/v1/items/details
 # https://catalog.roblox.com/v1/catalog/items/details
 
-cooldown = 60/(39/len(limiteds))
+cooldown = 60/(80/len(limiteds))
 while 1:
     start = time.perf_counter()
 
@@ -110,22 +153,29 @@ while 1:
                            json={"items": [{"itemType": "Asset", "id": int(limited)}]},
                            headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}).json()["data"][0]
         except KeyError:
-            print("Ratelimit! Waiting for next minute to start")
+            print(">> [LOGS] RATELIMITED WAITING FOR 60 SECONDS")
             time.sleep(60-int(datetime.datetime.now().second))
             continue
 
         if info.get("priceStatus", "") != "Off Sale":
-            productid = r.post("https://apis.roblox.com/marketplace-items/v1/items/details",
-                   json={"itemIds": [info["collectibleItemId"]]},
-                   headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}).json()[0]["collectibleProductId"]
+            try:
+                productid = r.post("https://apis.roblox.com/marketplace-items/v1/items/details",
+                    json={"itemIds": [info["collectibleItemId"]]},
+                    headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}).json()[0]["collectibleProductId"]
 
-            buy(info, info["collectibleItemId"], productid)
+                buy(info, info["collectibleItemId"], productid)
+            except:
+                print(">> [LOGS] CAUGHT ERRROR")
+                continue
 
     taken = time.perf_counter()-start
     if taken < cooldown:
         time.sleep(cooldown-taken)
 
     os.system("cls")
-    print("Check done.\n"
-          f"Time taken: {round(time.perf_counter()-start, 3)}\n"
-          f"Ideal time: {round(cooldown, 3)}")
+    print(">> [CREDITS] Made with love by noahrepublic, J3ldo, slourn, pompompurin, and duckyy. \n https://discord.gg/XX6qtbvkHG")
+    print("Also to the people who hacked our server: your a bitch :)")
+    print(">> [LOGS] CHECK DONE\n"
+          f"TIME TAKEN: {round(time.perf_counter()-start, 3)}\n"
+          f"IDEAL TIME: {round(cooldown, 3)}")
+    
